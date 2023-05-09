3d3b2ec8ba148a6de6532dd665ac1ad3488d5e54
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-05-09 21:35:00 +0500

    Create entry point add Classes

---
+++ main.rb
@@ -0,0 +1,9 @@
+require_relative './Classes/app'
+
+def main
+    puts '********-------------------------********'
+    puts '       Welcom to  Catalog of Things      '
+    puts "********-------------------------********\n"
+    App.new
+end
+main
