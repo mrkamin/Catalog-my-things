4d29f3d54e9a226306f6707c08d37c52e20869de
Francis Uche <francisuche712@gmail.com>
2023-05-11 19:34:57 +0100

    Update music.rb

--- Classes/music.rb
+++ Classes/music.rb
@@ -1,15 +1,29 @@
-require './item'
 
-class Music < Item
-  attr_accessor :aname, :on_spotify
+ require_relative './item'
+ 
+ class Music < Item
+   attr_accessor :name, :on_spotify
+ 
+   def initialize(publish_date, name, on_spotify)
+     super(publish_date)
+     @name = name 
+     @on_spotify = on_spotify
+   end
+   def as_hash
+     {
+       'id' => @id,
+       'publish_date' => @publish_date,
+       'name' => @name,
+       'archived' => @archived,
+       'on_spotify' => @on_spotify
+     }
+   end
+   def can_be_archived?
+     super && on_spotify
+   end
+ end
 
-  def initialize(aname, on_spotify, publish_date)
-    @aname = aname
-    @on_spotify = on_spotify
-    super(publish_date)
-  end
+ 
 
-  def can_be_archived?
-    super && on_spotify
-  end
-end
+   
+
