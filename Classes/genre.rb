2c394cd26b90b8eff00dae6deb1482669da1585e
official-wisdom <wisdomowuamanam@outlook.com>
2023-05-11 23:24:18 +0100

    fixed errors and update files

--- Classes/genre.rb
+++ Classes/genre.rb
@@ -1,24 +1,4 @@
-require './item'
-
-
 class Genre
   attr_accessor :name, :items
   attr_reader :id
-
-  def initialize(name)
-    @id = Random.rand(1..100)
-    @name = name
-    @items = []
-  end
-
-  def add_item(item)
-    return if @items.include?(item)
-
-    @items << item
-    item.genre = self
-  end
-
-  def self.all
-    ObjectSpace.each_object(self).to_a
-  end
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,6 @@ require_relative './Classes/app'
 require_relative './modules/menu'
 
 def main
-  include Menu
-
   puts '********-------------------------********'
   puts '       Welcome to  Catalog of Things      '
   puts "********-------------------------********\n"
