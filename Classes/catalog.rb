46d4fa26cba3eeac4da51170930f811b440c8178
Francis Uche <francisuche712@gmail.com>
2023-05-11 19:28:38 +0100

    Update catalog.rb

--- Classes/catalog.rb
+++ Classes/catalog.rb
@@ -1,11 +1,19 @@
-class Catalog
-  attr_reader :books
 
-  def initialize
-    @books = []
-  end
 
-  def add_book(book)
-    @books << book
-  end
-end
+ class Catalog
+   attr_reader :musics
+ 
+   def initialize
+     @musics = []
+   end
+ 
+   def add_music(music)
+     @musics << music
+   end
+ end
+
+ 
+
+
+
+
